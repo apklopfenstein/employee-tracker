b9d7f874f3141b574ba39f6f2e48c35b31eedcf0
Alyssa Klopfenstein <alyssa.r.purdy@gmail.com>
2020-10-17 23:04:09 -0500

    add inquirer functions

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,25 @@
+DROP DATABASE IF EXISTS employee.db;
+CREATE DATABASE employee.db;
+USE employee.db;
+
 CREATE TABLE departments (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER
+    salary DECIMAL (10, 2) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     rold_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (rold_id) REFERENCES role(id) ON DELETE CASCADE
 );
